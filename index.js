550e31103b5a29a577ac7abe0cb9d8c4e8d85143
aman githinji <amangithinji09@gmail.com>
2023-07-21 20:36:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    streetAddress: "collins",
+    name: "Sam",
+}
+function updateEmployeeWithKeyAndValue() {
+    let newEmployee = {...employee};
+    newEmployee["streetAddress"]="11 Broadway";
+    return newEmployee;
+}
+ 
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee["streetAddress"]="12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey() {
+ let newPloyee = {...employee};
+ newPloyee["name"]=undefined;
+ return newPloyee;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    let newLoyee = employee;
+    newLoyee["name"] = undefined;
+    return newLoyee;
+}
